0621da15c4975ae941e87d645a14e58cabd091b9
cheny <cheyeon001214@naver.com>
2025-08-02 01:25:07 +0900

    숫자고르기/골드5

---
+++ codus/week05/BOJ_2668_cy.java
@@ -0,0 +1,42 @@
+package codus.week05;
+
+import java.util.*;
+ 
+public class BOJ_2668_cy {    
+    
+    static ArrayList<Integer> list;
+    static boolean[] visited;
+    static int[] num;
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int n = scan.nextInt();
+        num = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            num[i] = scan.nextInt();
+        }
+        
+        list = new ArrayList<>();
+        visited = new boolean[n + 1];
+        for(int i = 1; i <= n; i++) {
+            visited[i] = true;
+            dfs(i, i);
+            visited[i] = false;
+        }
+        
+        System.out.println(list.size());
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }    
+    
+    public static void dfs(int start, int target) {
+        if(visited[num[start]] == false) {
+            visited[num[start]] = true;
+            dfs(num[start], target);
+            visited[num[start]] = false;
+        }
+        if(num[start] == target) list.add(target); 
+    }
+}
